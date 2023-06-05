219a6640a2805ddecb1a214177eb466f63bbd4f3
vinayjain2408 <vinay98292jain@gmail.com>
2023-06-05 14:09:09 +0530

    second commit

--- src/RouteRevise/Services.js
+++ src/RouteRevise/Services.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div>
+      <h1>Full Range of PCB Services to Fit All of Your Printed Circuit Board Demands</h1>
+      <p>We are a one-stop printed circuit board manufacturer, quick-turn PCB assembly and design solution provider for electronics manufacturing. Offering some of the most art-of-edge PCB technology and convenient manufacturing and assembly services found in the industry. Full Spec PCBs, Low-Volume Express PCBs, Multilayer Boards, HDI PCBs.</p>
+    </div>
   )
 }
 
